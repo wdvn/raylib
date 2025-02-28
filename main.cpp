253c07f3ade68a48199c1dd376258bec8e717c3a
luynt <luynt@fpt.com.vn>
2025-02-28 10:50:04 +0700

    start project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+int main() {
+  InitWindow(800, 600, "raylib example");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, raylib!", 10, 10, 20, BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
+
+//compile
+//clang++ main.cpp -o main -I./include -L./lib -lraylib -lGL -lX11 -lpthread -lm -ldl -Wl,-rpath,./lib
